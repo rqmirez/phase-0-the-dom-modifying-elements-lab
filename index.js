ff097a015e81e0f9731606533c7a98bc1448f41b
Judith Ramirez <rqmirez@gmail.com>
2022-06-10 13:28:27 -0400

    commit tree 79dd5a82581974a2f89aa5203a5b4f70204bdc9c

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const element = document.querySelector("main").remove()
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+
+newHeader.innerHTML = "JUDI is the champion"
